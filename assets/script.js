163e537b39dc914834b90889def24a4ddeee9285
Flowers <angela.banks25@gmail.com>
2020-01-11 15:15:03 -0800

    new

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,54 @@
-//Weather Single Day API
-  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}, {country code}&AAPID=ca41c4eea8b5971b71a7ea04a63fd171";
-  let searchedItem = document.querySelector("[data-searched");
-  let response = searchedItem
+//Run when page loads
 
+$(document).ready(function () {
+    $("#submit").onload("click", function () {
+        event.preventDefault();
+//Variable that will return value from searchbar
+        let searchbar = $(".searchbar").value();
 
-  let q= ""
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        $(".tempature").text(response.main.temp);
-    //Convert Temp
-        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $(".tempF").text("Temperature (Kelvin) " + tempF);
-        console.log(tempF);
+ //Return Search Results with API documentation
+        if (searchbar != '') {
+            
+            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&units=imperial" + "&APPID=ca41c4eea8b5971b71a7ea04a63fd171";
+            
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+            }).then(function(response){
+                console.log(response);
+        
+            //Populate Search Results
+                let row = $("<tr>");
+                let city = $("<th>").text(response.name);
+                let row1 = $("<tr>");
+                let temp = $("<td>").text(response.main);
+                let row2 = $("<tr>");
+                let humid = $("<td>").text(response.main.humidity);
+                let row3 = $("<tr>");
+                let wind = $("<td>").text(response.wind.speed);
+                //let row4 = $("<tr>");
+                //let uv = $("<td>").text(response.main.uv);
 
-        $(".humid").text(response.main.humidity);
-        $(".wind").text(response.wind.speed);
-        //$(".uv").text(response.);
+        // Append the td elements to the new table row
+                row.append(city);
+                row1.append(temp);
+                row2.append(humid);
+                row3.append(wind);
+                //row4.append(uv);
 
-    $(".container").append(text);
+      // Append the table row to the tbody element
+                $("tbody").append(row);
+                $("tbody").append(row1);
+                $("tbody").append(row2);
+                $("tbody").append(row3);
+                //$("tbody").append(row4);
+            });
+
+        }else {
+            $("#noblank").html("Cannot Be Blank")
+        }
     })
-     
+});
+
   
  
