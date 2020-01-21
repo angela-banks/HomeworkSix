718c6df7e00c62d6881fc0b741eac59071e26f9f
Flowers <angela.banks25@gmail.com>
2020-01-20 19:26:12 -0800

    uv

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,110 @@
 //Run when page loads
 
+const API_KEY = "ca41c4eea8b5971b71a7ea04a63fd171"
 $(document).ready(function () {
     $("#submit").on("click", function () {
         event.preventDefault();
-//Variable that will return value from searchbar
+        //Variable that will return value from searchbar
         let searchbar = $(".search").val();
         console.log(searchbar);
- //Return Search Results with API documentation
+        //Return Search Results with API documentation
         if (searchbar != '') {
-            
-            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&units=imperial" + "&APPID=ca41c4eea8b5971b71a7ea04a63fd171";
-           
+
+            let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&units=imperial" + "&APPID=" + API_KEY + "&units=imperial";
+
             $.ajax({
                 url: queryURL,
                 method: "GET",
-            }).then(function(response){
+            }).then(function (response) {
                 console.log(response);
-            // //Populate & Append Search Results
-                $("<city>").innerHTML = "response.name";
+                // //Populate & Append Search Results
+                $("#city").text("City: " + response.name);
                 console.log(response.name);
-                $("temp").innerHTML = "response.main.temp";
+                $("#temp").text(response.main.temp);
                 console.log(response.main.temp);
-                $("<humid>").innerHTML = "response.main.humidity";
-                $("wind").innerHTML = "response.wind.speed";
-                $("<uv>").innerHTML = "response.main.uv";
-                $("<icon>").innerHTML = "response.weather[0].icon";
+                $("#humid").text(response.main.humidity);
+                $("#wind").text(response.wind.speed);
+                $("#uv").text(response.main.uv);
+                $("#icon").text(response.weather[0].icon);
+                //Display weather icon per weather conditions
+                let image = $("<img>");
+                image.attr("src", `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
+                $("#icon").append(image);
+
+                //Variable that will return value from searchbar
+                // let searchbar = $(".searchbar").val();
+                console.log(searchbar);
+
+                //Return 5-Day Weather Search Results with API documentation
+
+                let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchbar + "&units=imperial" + "&APPID=" + API_KEY + "&units=imperial";
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET",
+                }).then(function (responseone) {
+                    console.log(responseone);
+                    let FiveForecast = [];
+                    let list = responseone.list;
+                    for (let i = 0; i < list.length; i++) {
+                        if (list[i].dt_txt.indexOf("00:00:00") > -1) {
+                            FiveForecast.push(list[i]);
+                        }
+                    }
+                    console.log(FiveForecast);
+                    for( let i=0; i < FiveForecast.length; i++){
+                        let card = $("<div>").attr("class", "card col-lg-2 p-1 card text-black bg-primary")
+                        let cardbody = $("<div>").attr("class", "card-body p-0 text-center")
+                        let date = $("<h5>").text(FiveForecast[i].dt_txt);
+                        date.attr("class", "card-title")
+                        let icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + FiveForecast[i].weather[0].icon + "@2x.png")
+                        let temp = $("<div>").text("temp: " + (FiveForecast[i].main.temp))
+                        let humidity = $("<div>").text("humidity: " + (FiveForecast[i].main.humidity))
+                        cardbody.append(date, icon, temp, humidity);
+                        card.append(cardbody)
+                        $(".Days").append(card);
+                    }
+                    //uv call
+                    let uv = "http://api.openweathermap.org/data/2.5/uvi?";
+                    let cor = "lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+                    let uvURL = uv + cor + "&APPID=" + API_KEY;
+                    console.log(uvURL);
+                    $.ajax({
+                        url: uvURL,
+                        method: "GET"
+                    }).then(function (response){
+                        console.log(response);
+                        let index = $("<mark>").text(response.value)
+                        index.attr("class", function(response){
+                            if (response.value < 4 ){
+                                return "text-success" 
+                            } else if (response.value < 8 ){
+                                return "text-warning"
+                            } else {
+                                return "text-danger"
+                            }
+                        })
+                        let UV = $("<div>").text("UV Index: ")
+                        UV.append(index)
+                        $(".container").append(UV)
+                        
+                    })
+                })
+
             });
 
-        }else {
+        } else {
             $("#noblank").html("Cannot Be Blank")
         }
     })
 
 
-    //Variable that will return value from searchbar
-    let searchbar = $(".searchbar").val();
-    console.log(searchbar);
 
- //Return 5-Day Weather Search Results with API documentation
- if (searchbar != '') {
-            
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchbar + "&units=imperial" + "&APPID=ca41c4eea8b5971b71a7ea04a63fd171";
-   
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(displayResults){
-        console.log(response);
-    })
 
-function displayResults ({
 
-})
 
-}
+
 
 });
 
 
-
-  
- 
